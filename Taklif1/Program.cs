4c4f2485e6cb3940beac8be2dc10a352d0d2ee9c
farnazshahshahani <farnazshahshahani664@gmail.com>
2023-02-24 23:17:30 +0330

    taklif-1

---
+++ Taklif1/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Taklif1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[ , ] arr = new int[10 , 10];
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.WriteLine("Enter number:");
+                    arr[i , j] = Int32.Parse(Console.ReadLine());
+                }
+            }
+
+
+            int x, y;
+            int n = 0;
+            int m = 0;
+            Console.WriteLine("Enter number x to search:");
+            x = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number y to search:");
+            y = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (arr[j , i] == x)
+                    {
+                        n++;
+                    }
+                    if (arr[j , i] == y)
+                    {
+                        m++;
+                    }
+                }
+            }
+
+            Console.WriteLine("x count is :" + n.ToString());
+            Console.WriteLine("y count is :" + m.ToString());
+        }
+    }
+}
---
+++ Taklif1_2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Taklif1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine("Enter number:");
+                    arr[i, j] = Int32.Parse(Console.ReadLine());
+                }
+            }
+        }
+    }
+}
